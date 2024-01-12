2b0da2266ea04a925a1efc2c7e0e1501bec6beff
maaznizam290 <maaznizam290@gmail.com>
2024-01-13 01:02:14 +0500

    created a structure of mvc

---
+++ TodoApp/Models/Todo.cs
@@ -0,0 +1,7 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+namespace TodoApp.Models;
+
+public class Todo{
+    public Guid ID {get; set;}          // GUID => represents the global 
+}
